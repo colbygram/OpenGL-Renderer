7f11cd5965627bef4cdef589f1a470fa3f81c500
Colby Gram <cgram79@yahoo.com>
2025-10-24 23:05:19 -0700

    update glad

--- OpenGL Renderer/OpenGL Renderer/src/main.cpp
+++ OpenGL Renderer/OpenGL Renderer/src/main.cpp
@@ -1,9 +1,9 @@
-#define GLAD_GL_IMPLEMENTATION
-#include <glad/gl.h>
-
 #define GLFW_DLL
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/gl.h>
 
 int main(int argc, char* argv[]) {
 
