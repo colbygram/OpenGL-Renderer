0a800976a713e1defb3cc3c40a033faa15d9b2e9
Colby Gram <cgram79@yahoo.com>
2026-02-06 14:59:23 -0800

    Update main.cpp

--- OpenGL Renderer/OpenGL Renderer/src/main.cpp
+++ OpenGL Renderer/OpenGL Renderer/src/main.cpp
@@ -209,12 +209,9 @@ int main(void) {
 		glClear(GL_COLOR_BUFFER_BIT);
 		//After setting up VAO, VBO and shaders to use those, we can finally render triangle to screen
 		glUseProgram(program_id);
-		//identity 4x4 matrix
-		glm::mat4 transform(1.0f);
-		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
-		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(1.0f, 0.0f, 0.0f));
-		//transform = glm::rotate(transform, (float)glfwGetTime() , glm::vec3(0.0f, 1.0f, 0.0f));
+		//Identity 4x4 matrix
+		 glm::mat4 transform = glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, -0.5f, 0.0f)) * glm::rotate(glm::mat4(1.0f), (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
+
 		glUniformMatrix4fv(glGetUniformLocation(program_id, "transform"), 1, GL_FALSE, glm::value_ptr(transform));
 		
 		glActiveTexture(GL_TEXTURE0);
